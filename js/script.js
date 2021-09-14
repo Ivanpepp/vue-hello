54e73fd289cd25ba7e099a50e636d59d8d3e6578
Ivanpepp <nastroivan3@gmail.com>
2021-09-14 16:23:19 +0200

    fixed

--- js/script.js
+++ js/script.js
@@ -10,13 +10,26 @@ let app = new Vue(
             subTitle: 'mt-5 pb-2 fs-3',
             bottoneSegreto: 'd-none',
             value : '',
+            bottoneSegreto2: 'd-none',
+            bottoneSegreto3: 'd-none',
+            suggerimento: 'd-none',
         },
         methods: {
              appari: function (){
                 this.bottoneSegreto = 'd-block';
+                this.bottoneSegreto2 = 'd-block';
+                this.bottoneSegreto3 = 'd-block';
+                this.suggerimento = 'd-block';
             },
             ruota: function(){
-                this.title += ' animate__flip animate__animated';
+                this.title = ' animate__flip animate__animated text-danger fs-1 text-center m-5';
+            },
+            hinge: function(){
+                this.subTitle = ' animate__flip animate__hinge mt-5 pb-2 fs-3';
+            },
+            clear: function(){
+                this.subTitle = 'mt-5 pb-2 fs-3';
+                this.title = 'text-danger fs-1 text-center m-5';
             }
         },
       
